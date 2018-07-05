a88b36db90c2858a11bc2dc4986c99d53db68ea9
Dimas <dimas.ciputra@gmail.com>
2018-07-05 14:29:14 +0700

    Login required, fix upload error

--- fish/urls.py
+++ fish/urls.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 from fish.api_views.fish_collection_record import (
     FishCollectionList,
     FishCollectionDetail,
@@ -12,7 +13,9 @@ api_urls = [
     url(r'^api/fish-collections/$', FishCollectionList.as_view()),
     url(r'^api/fish-collections/(?P<pk>[0-9]+)/$',
         FishCollectionDetail.as_view()),
-    url(r'^fish/upload/$', CsvUploadView.as_view(), name='fish-csv-upload'),
+    url(r'^fish/upload/$',
+        login_required(CsvUploadView.as_view()),
+        name='fish-csv-upload'),
 ]
 
 urlpatterns = [
--- fish/views/csv_upload.py
+++ fish/views/csv_upload.py
@@ -3,6 +3,7 @@
 """
 
 import csv
+import sys
 from datetime import datetime
 from django.urls import reverse_lazy
 from django.contrib.gis.db import models
@@ -85,7 +86,7 @@ class CsvUploadView(FormView):
 
                     # Get existed taxon
                     collections = FishCollectionRecord.objects.filter(
-                            original_species_name=record['Species']
+                            original_species_name=record['species_name']
                     )
 
                     taxon_gbif = None
@@ -95,7 +96,14 @@ class CsvUploadView(FormView):
                     # Optional fields and value
                     optional_records = {}
 
-                    for (opt_field, field_type) in optional_fields.iteritems():
+                    if (sys.version_info > (3, 0)):
+                        # Python 3 code in this block
+                        optional_fields_iter = optional_fields.items()
+                    else:
+                        # Python 2 code in this block
+                        optional_fields_iter = optional_fields.iteritems()
+
+                    for (opt_field, field_type) in optional_fields_iter:
                         if opt_field in record:
                             if field_type == 'bool':
                                 record[opt_field] = record[opt_field] == '1'

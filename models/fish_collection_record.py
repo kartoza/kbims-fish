7953d5c25d4a42433665db60c1bfe030e49cf93e
Dimas <dimas.ciputra@gmail.com>
2018-09-19 12:18:32 +0700

    Add new fields for fish model

--- models/fish_collection_record.py
+++ models/fish_collection_record.py
@@ -22,6 +22,60 @@ class FishCollectionRecord(BiologicalCollectionRecord):
         blank=True,
     )
 
+    # Depth (cm)
+    depth_cm = models.FloatField(
+        null=True,
+        blank=True
+    )
+
+    # Near Bed Velocity
+    near_bed_velocity = models.CharField(
+        max_length=100,
+        blank=True
+    )
+
+    # Substrate
+    substrate = models.CharField(
+        max_length=100,
+        blank=True,
+    )
+
+    # EC (μS/cm)
+    ec = models.FloatField(
+        null=True,
+        blank=True
+    )
+
+    # pH
+    ph = models.FloatField(
+        null=True,
+        blank=True
+    )
+
+    # DO
+    do = models.FloatField(
+        null=True,
+        blank=True
+    )
+
+    # Temp (°C)
+    temp = models.CharField(
+        max_length=100,
+        blank=True
+    )
+
+    # Turbidity (NTU)
+    turbidity = models.CharField(
+        max_length=100,
+        blank=True
+    )
+
+    # Nutrients
+    nutrients = models.CharField(
+        max_length=100,
+        blank=True
+    )
+
     # noinspection PyClassicStyleClass
     class Meta:
         """Meta class for project."""

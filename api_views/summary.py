42fe41120d742fd8ebc811b19e280f2cc330a077
Anita Hapsari <anita@kartoza.com>
2018-10-16 12:38:57 +0700

    add fish summary api (#13)
    * add fish summary api
    
    * fix api
    
    * clean data
    
    * clean api code

---
+++ api_views/summary.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+import json
+from django.db.models import Count
+from fish.models.fish_collection_record import FishCollectionRecord
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class FishSummary(APIView):
+
+    def get(self, request, *args):
+        category = 'category'
+        fish_summary = \
+            FishCollectionRecord.objects.values(
+                category).annotate(total=Count(category))
+
+        response = {
+            category: {}
+        }
+
+        for summary in fish_summary:
+            class_name = summary[category]
+            if not class_name:
+                continue
+
+            response[category][class_name] = summary['total']
+
+        response['total_fish'] = FishCollectionRecord.objects.all().count()
+
+        return Response(response)
